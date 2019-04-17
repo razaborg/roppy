360d091bda459ab92b8f53cc4b86e1b7d5de90ba
razaborg <contact@razaborg.fr>
2019-04-17 13:51:32 +0200

    changed the list types to set and tuple (depending of the needs) and moved the query directly into the backward_disas function

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def findall_ret(data):
         last_ret = x
     
 
-def backward_disas(data, offset, n, section_offset, maxlen=8, symtab=None):
+def backward_disas(data, offset, n, section_offset, maxlen=8, symtab=None, mnemonic_query=None, register_query=None):
     '''
     This function dissasemble a bytecode which ends at a specific @offset.
     The @n parameter gives how many bytes back must be taken at max.
@@ -33,22 +33,30 @@ def backward_disas(data, offset, n, section_offset, maxlen=8, symtab=None):
             address = (offset-i) + section_offset
             for inst in pydis.decode(bytecode, address=address):
                 instructions.append(inst)
+            
+            g = Gadget(tuple(instructions), maxlen, symtab)
+            
+            
+            if mnemonic_query is not None:
+                if not g.has_mnemonic(args.mnemonic):
+                    del g
+                    continue
+            if register_query is not None:
+                if not g.has_register(args.register):
+                    del g
+                    continue
+            gadgets.append(g)
         except Exception:
             continue
         
-        try: 
-            gadgets.append(Gadget(instructions, maxlen, symtab))
-        except InvalidGadget:
-            continue
-
     return gadgets
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Look for interesting gadgets inside a binary.')
     parser.add_argument('file', type=str, help='The binary file to inspect')
-    parser.add_argument('-m', '--mnemonic', help='Search for a specific mnemonic')
-    parser.add_argument('-r', '--register', help='What register you want to manipulate.')
+    parser.add_argument('-m', '--mnemonic', default=None, type=str, help='Search for a specific mnemonic')
+    parser.add_argument('-r', '--register', default=None, type=str, help='What register you want to manipulate.')
     parser.add_argument('-l', '--max-len', type=int, help='Maximum lenght of gadgets found. (default to 8)', default=8)
     parser.add_argument('-b', '--bytes-backward', type=int, default=30, help='Number of bytes to browse backwards each time a ret instruction if found. (default to 30)')
     parser.add_argument('-s', '--symbols', action='store_true', default=False, help='Try to resolve symbols (prototypal only .symtab for now).')
@@ -74,22 +82,20 @@ if __name__ == '__main__':
     # look into the .text section to find all interesting ending-gadgets opcodes
     ret_offsets = findall_ret(text_data)
     
-    gadgets = []
+    gadgets = set()
     # for each 'ret' opcode we found in the binary
     # we proceed backwards to find new gadgets
     for ret in ret_offsets:
-        gadget = backward_disas(text_data, ret, args.bytes_backward, \
+        gadget = set(backward_disas(text_data, ret, args.bytes_backward, \
                 section_offset=text_section.header.sh_addr, \
-                maxlen=args.max_len, symtab=symtab)
-        gadgets.extend(gadget)
-
-    # and after all of this we have a complete list of gadgets :-) 
-    for gadget in gadgets:
-        if args.mnemonic:
-            if not gadget.has_mnemonic(args.mnemonic):
-                continue
-        if args.register:
-            if not gadget.has_register(args.register):
-                continue
-                
+                maxlen=args.max_len, symtab=symtab, \
+                register_query=args.register, mnemonic_query=args.mnemonic))
+        # the global gadget set is update to conserve all the elements but keep only
+        # the unique gadgets
+        gadgets.update(gadget)
+    
+    # and after all of this we have a complete set of unique gadgets :-) 
+    for gadget in gadgets:                
         print('{} : {}'.format(gadget.address, gadget.to_string()))
+    print()
+    print('{} unique gadgets found.'.format(len(gadgets)))
